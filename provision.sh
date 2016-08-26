5cabf0e1b43eb338188d631c98c5b42601c0820f
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-08-26 18:28:10 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -4,11 +4,16 @@ cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
 sudo dpkg -i chrome.deb || sudo apt-get -f install -y
 
+sudo rm /var/lib/dpkg/lock
+
 wget https://atom.io/download/deb -O atom.deb
 sudo dpkg -i atom.deb || sudo apt-get -f install -y
+
+sudo rm /var/lib/dpkg/lock
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
 
 source ~/.bashrc
 
 nvm install node
+
