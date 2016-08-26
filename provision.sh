8f912d1c68f8502c71092ec801d58d772cbc8004
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-08-26 18:49:54 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -13,7 +13,19 @@ sudo rm /var/lib/dpkg/lock
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
 
 sudo rm /var/lib/dpkg/lock
 
 sudo npm install -g gulp
+
+echo '
+cd ~/Code || mkdir ~/Code && cd ~/Code
+parse_git_branch() {
+  git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/ (\1)/"
+}
+export PS1="\u \W\[\033[36m\]\$(parse_git_branch)\[\033[00m\] $ "
+alias git="echo Use Git on your host machine. You cannot run git "
+' >> ~/.bashrc
+
+source ~/.bashrc
