0065d57e041cdd8ee368b4d65536cc127c2b4990
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-08-27 17:21:15 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -126,4 +126,5 @@ echo '"*":
 ' > ~/.atom/config.cson 
 
 google-chrome --disable-gpu 'http://localhost:8000'&
+google-chrome --disable-gpu 'https://github.com/verekia?tab=repositories'&
 atom ~/Code --disable-gpu&
