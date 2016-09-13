9aaaf0b9006fac01a64db98304d23b8d92a26ebb
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-09-13 12:13:56 +0200

    Update provision-termux.sh

--- provision-termux.sh
+++ provision-termux.sh
@@ -1,5 +1,5 @@
 # Run this script by pasting this into a terminal on a fresh new Termux install
-# sudo wget -O - https://raw.githubusercontent.com/verekia/dev-provisioning/master/provision.sh | bash
+# sudo wget -O - https://raw.githubusercontent.com/verekia/dev-provisioning/master/provision-termux.sh | bash
 
 apt update && apt upgrade
 # vim, vundle, vimrc, git , git config, nvm, Node latest, https://github.com/mtscout6/syntastic-local-eslint.vim/blob/master/README.md
