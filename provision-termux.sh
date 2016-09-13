387f304c019fa96f1bd1ade5a37abc0610b58ff5
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-09-13 12:40:38 +0200

    Update provision-termux.sh

--- provision-termux.sh
+++ provision-termux.sh
@@ -1,16 +1,20 @@
-# Run this script by pasting this into a terminal on a fresh new Termux install
-# sudo wget -O - https://raw.githubusercontent.com/verekia/dev-provisioning/master/provision-termux.sh | bash
+# apt update
+# apt upgrade -y
+# apt install -y curl
 
-apt update && apt upgrade -y
+# Run this script by pasting this into a terminal on a fresh new Termux install
+# curl https://raw.githubusercontent.com/verekia/dev-provisioning/master/provision-termux.sh | bash
+
 # git config, nvm, Node latest, https://github.com/mtscout6/syntastic-local-eslint.vim/blob/master/README.md
 # add aliases like git s for git status
 
 apt install -y git
 apt install -y vim
+apt install nodejs -y
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo '
+echo "
 set nocompatible
 filetype off
 
@@ -24,6 +28,10 @@ call vundle#end()
 filetype plugin indent on
 syntax enable
 colorscheme monokai
-' >> ~/.vimrc
+" >> ~/.vimrc
+
+vim +PluginInstall +qall
+
+
 
 echo 'Press Volume Up + Q for extra keys'
