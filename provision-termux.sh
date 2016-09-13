c77df2a2fbac404c7e80109b86d17e177aa0ffa4
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-09-13 12:15:40 +0200

    Update provision-termux.sh

--- provision-termux.sh
+++ provision-termux.sh
@@ -3,3 +3,4 @@
 
 apt update && apt upgrade
 # vim, vundle, vimrc, git , git config, nvm, Node latest, https://github.com/mtscout6/syntastic-local-eslint.vim/blob/master/README.md
+# add aliases like git s for git status
