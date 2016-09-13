e804e2f9cdee220339d49b1790e0115f52b86324
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-09-13 12:23:16 +0200

    Update provision-termux.sh

--- provision-termux.sh
+++ provision-termux.sh
@@ -2,7 +2,28 @@
 # sudo wget -O - https://raw.githubusercontent.com/verekia/dev-provisioning/master/provision-termux.sh | bash
 
 apt update && apt upgrade
-# vim, vundle, vimrc, git , git config, nvm, Node latest, https://github.com/mtscout6/syntastic-local-eslint.vim/blob/master/README.md
+# git config, nvm, Node latest, https://github.com/mtscout6/syntastic-local-eslint.vim/blob/master/README.md
 # add aliases like git s for git status
+
+apt install -y git
+apt install -y vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo '
+set nocompatible
+filetype off
+
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'pangloss/vim-javascript'
+Plugin 'scrooloose/syntastic'
+Plugin 'sickill/vim-monokai'
+call vundle#end()
+filetype plugin indent on
+syntax enable
+colorscheme monokai
+' >> ~/.vimrc
 
 echo 'Press Volume Up + Q for extra keys'
