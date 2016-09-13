e41b49029f8006bab68596bdabe5d7a7fc512f0f
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-09-13 12:57:30 +0200

    Update provision-termux.sh

--- provision-termux.sh
+++ provision-termux.sh
@@ -1,9 +1,5 @@
-# apt update
-# apt upgrade -y
-# apt install -y curl
-
-# Run this script by pasting this into a terminal on a fresh new Termux install
-# curl https://raw.githubusercontent.com/verekia/dev-provisioning/master/provision-termux.sh | bash
+# Run this script by pasting this command into a terminal on a fresh new Termux install
+# apt update && apt upgrade -y && apt install -y curl && curl https://raw.githubusercontent.com/verekia/dev-provisioning/master/provision-termux.sh | bash
 
 # git config, nvm, Node latest, https://github.com/mtscout6/syntastic-local-eslint.vim/blob/master/README.md
 # add aliases like git s for git status
