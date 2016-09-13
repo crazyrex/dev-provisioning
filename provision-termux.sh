7ab9ed17be66e8921227cefaac8076a575c706db
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-09-13 15:04:37 +0200

    Update provision-termux.sh

--- provision-termux.sh
+++ provision-termux.sh
@@ -39,6 +39,17 @@ call vundle#end()
 filetype plugin indent on
 syntax enable
 silent! colorscheme monokai
+
+let g:syntastic_javascript_checkers = ['eslint']
+let g:syntastic_javascript_eslint_exec = './node_modules/.bin/eslint'
+
+set statusline+=%#warningmsg#
+set statusline+=%{SyntasticStatuslineFlag()}
+set statusline+=%*
+let g:syntastic_always_populate_loc_list = 1
+let g:syntastic_loc_list_height = 3
+let g:syntastic_auto_loc_list = 0
+let g:syntastic_check_on_open = 0
 " >> ~/.vimrc
 
 echo '
