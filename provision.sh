eae632043b36a298540787630d4bd94ff77e1b2f
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-09-14 18:04:22 +0200

    Added MongoDB

--- provision.sh
+++ provision.sh
@@ -26,6 +26,13 @@ sudo dpkg -i atom.deb
 Status 'sudo apt-get -f install -y' 'Atom'
 
 DeleteLock
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+Status 'sudo apt-get install -y mongodb-org' 'MongoDB'
+Status 'sudo service mongod start' 'Started MongoDB'
+
+DeleteLock
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 Status 'sudo apt-get install -y nodejs' 'Node'
 DeleteLock
